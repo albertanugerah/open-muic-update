fa5f6ea1e9e522f49844ec1f1b50c544d02ee2f3
Albert Anugerah Putra Siregar <albert.siregar@mitra.jasamarga.co.id>
2022-06-05 14:29:23 +0700

    authentications implemented

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 // albums
 const albums = require('./api/albums');
 const AlbumsService = require('./services/postgres/AlbumsService');
@@ -11,12 +12,23 @@ const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
 const SongsValidator = require('./validators/songs');
 
-// const ClientError = require('./exceptions/ClientError');
-// const NotFoundError = require('./exceptions/NotFoundError');
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validators/users');
+
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validators/authentications');
 
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
+  const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -25,6 +37,27 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+  // mendefinisikan strategy autentikasi jwt
+  server.auth.strategy('open_music', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
   await server.register([
     {
@@ -40,35 +73,22 @@ const init = async () => {
         service: albumsService,
         validator: AlbumsValidator,
       },
+    }, {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    }, {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
+      },
     }]);
-  // server.ext('onPreResponse', (request, h) => {
-  //   const { response } = request;
-  //
-  //   if (response instanceof ClientError) {
-  //     const newResponse = h.response({
-  //       status: 'fail',
-  //       message: response.message,
-  //     });
-  //     newResponse.code(response.statusCode);
-  //     return newResponse;
-  //   }
-  //   if (response instanceof NotFoundError) {
-  //     const newResponse = h.response({
-  //       status: 'fail',
-  //       message: response.message,
-  //     });
-  //     newResponse.code(response.statusCode);
-  //     return newResponse;
-  //   }
-  //   // Server ERROR!
-  //   const newResponse = h.response({
-  //     status: 'error',
-  //     message: 'Maaf, terjadi kegagalan pada server kami.',
-  //   });
-  //   newResponse.code(500);
-  //
-  //   return response.continue || response;
-  // });
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
